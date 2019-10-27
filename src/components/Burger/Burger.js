aef96be46cbdcbde4c72b5bb03adf78974f314b1
Vano-Stas <st.ivan727@gmail.com>
2019-10-27 22:17:12 +0300

    Add modal, ordersummary components

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,17 +4,23 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.css';
 
 const Burger = (props) => {
-    const transformedIngedients = Object.keys(props.ingredients)
+    let transformedIngedients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={ igKey + i } type={ igKey } />
             }); 
-        });
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+        if (transformedIngedients.length === 0) {
+            transformedIngedients = <p>Please start adding ingredients!</p>
+        }
+    console.log(transformedIngedients);
     return (
         <div className={ classes.Burger }>
             <BurgerIngredient type="bread-top" />
-            <BurgerIngredient type="cheese" />
-            <BurgerIngredient type="meat" />
+            {transformedIngedients}
             <BurgerIngredient type="bread-bottom" />
         </div>
     );
