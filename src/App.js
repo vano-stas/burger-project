9829fc7a88c69c5fc7dcc76e32ca87695801b215
Vano <st.ivan727@gmail.com>
2019-11-10 22:12:45 +0300

    Adding react router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from './huk/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 
 class App extends Component {
@@ -8,7 +10,12 @@ class App extends Component {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
+          <Switch>
+            <Route path='/' exact component={BurgerBuilder} />
+            <Route path='/checkout' component={Checkout} />
+          </Switch>
+          
+          <Checkout />
         </Layout>
       </div>
     );
