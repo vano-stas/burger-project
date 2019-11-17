ef02504214aaab1433c46312026bc033d5f7f9c7
Vano <st.ivan727@gmail.com>
2019-11-17 14:44:54 +0300

    Add form validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,7 +3,10 @@ import React from 'react';
 import classes from './Button.css';
 
 const Button = (props) => (
-    <button onClick={ props.clicked } className={ [classes.Button, classes[props.btnType]].join(' ') }>
+    <button 
+        onClick={ props.clicked }
+        className={ [classes.Button, classes[props.btnType]].join(' ') }
+        disabled={props.disabled}>
         {props.children}
     </button>
 );
