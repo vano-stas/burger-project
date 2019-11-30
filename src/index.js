388b8421e1ed7fc5e4c4f4d3400bff188ef09b0d
Vano <st.ivan727@gmail.com>
2019-12-01 00:24:35 +0300

    Add advanced redux in project

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 
 
 import './index.css';
 import App from './App';
 import burgerBuilderReducer from './store/reducer/burgerBuilder';
+import orderReducer from './store/reducer/order'; 
 
 // import * as serviceWorker from './serviceWorker';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(burgerBuilderReducer, composeEnhancers(applyMiddleware(thunk)));
+const rootReducer = combineReducers({
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+});
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from '../../axios-orders';
 
 export const purchaseBurgerSuccess = (id, orderData) => {
   return {
@@ -12,5 +13,71 @@ export const purchaseBurgerFail = (error) => {
   return {
     type: actionTypes.PURCHASE_BURGER_FAIL,
     error: error
+  }
+};
+
+export const purchaseBurgerStart = () =>{
+  return {
+    type: actionTypes.PURCHASE_BURGER_START
+  }
+}
+
+export const purchaseBurger = (orderData) => {
+  return dispatch => {
+    dispatch(purchaseBurgerStart());
+    axios.post('/orders.json', orderData)
+      .then(response => {
+        console.log(response.data);
+        dispatch(purchaseBurgerSuccess(response.data.name, orderData))
+      })
+      .catch(error => {
+        dispatch(purchaseBurgerFail(error))
+      })
+  };
+};
+
+export const purchaseInit = () => {
+  return {
+    type: actionTypes.PURCHASE_INIT
+  };
+};
+
+export const fetchOrdersSuccess = (orders) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_SUCCESS,
+    orders: orders
+  };
+};
+
+export const fetchOrdersFail = (error) => {
+  return {
+    type: actionTypes.FETCH_ORDERS_FAIL,
+    error: error
+  };
+};
+
+export const fetchOrdersStart = () => {
+  return {
+    type: actionTypes.FETCH_ORDERS_START
+  };
+};
+
+export const fetchOrders = () => {
+  return dispatch => {
+    dispatch(fetchOrdersStart());
+    axios.get('/orders.json')
+      .then(res => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key
+          });
+        }
+        dispatch(fetchOrdersSuccess(fetchedOrders));
+      })
+      .catch(err => {
+        dispatch(fetchOrdersFail(err))
+      })
   }
 }
