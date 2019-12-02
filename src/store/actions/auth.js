8273212e55b99a484979acc636207eb3e8194bd3
Vano <st.ivan727@gmail.com>
2019-12-02 22:23:09 +0300

    Improve redirect logic

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,8 @@ export const authFail = (error) => {
 };
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('expirationDate');
   return {
     type: actionTypes.AUTH_LOGOUT
   }
@@ -51,11 +53,27 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
+        const expirationDate = new Date(new Date().getTime() + response.dataexpiresIn * 1000);
+        localStorage.setItem('token', response.data.idToken);
+        localStorage.setItem('expirationDate', expirationDate);
         dispatch(authSuccess(response.data.idToken, response.data.localId));
         dispatch(checkAuthTimeout(response.data.expiresIn))
       })
       .catch(err => {
         dispatch(authFail(err.response.data.error))
       })
+  };
+};
+
+export const setAuthRedirectPath = (path) => {
+  return {
+    type: actionTypes.SET_AUTH_REDIRECT_PATH,
+    path: path
+  };
+};
+
+export const authCheckState = () => {
+  return dispatch => {
+    
   };
 };
