331c56c506771e65c0ae097bb77d83a1e74a221a
Vano <st.ivan727@gmail.com>
2019-12-03 21:26:22 +0300

    Add specific data for user

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './huk/Layout/Layout';
@@ -17,23 +17,41 @@ class App extends Component {
   }
 
   render() {
+    let routes = (
+      <Switch>
+        <Route path='/' exact component={BurgerBuilder} />
+        <Route path= '/auth' component={Auth} />
+        <Redirect to='/' />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path='/' exact component={BurgerBuilder} />
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path= '/logout' component={Logout} />
+          <Redirect to='/' />
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path='/' exact component={BurgerBuilder} />
-            <Route path='/checkout' component={Checkout} />
-            <Route path='/orders' component={Orders} />
-            <Route path= '/auth' component={Auth} />
-            <Route path= '/logout' component={Logout} />
-          </Switch>
-
-          {/* <Checkout /> */}
+          {routes}
         </Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -41,4 +59,4 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
